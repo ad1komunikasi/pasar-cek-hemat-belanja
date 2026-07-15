fd8c94fda402bccc0b340ed465b28a46dbc448ce
Adhimono W <adhi.ciptakomunikasi@gmail.com>
2026-07-16 03:29:26 +0700

    feat: add branded SVG favicon and update route document head configuration

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -117,6 +117,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:image", content: "/twitter-image.jpg" },
     ],
     links: [
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
